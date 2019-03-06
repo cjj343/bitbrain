af52446070ce82a8102e8de1a9fd7eeb2cbebf69
Cameron Jackson <cameron.jackson@accenturefederal.com>
2019-03-06 09:01:34 -0600

    image functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from './Components/Navigation/Navigation.js'
-import Logo from './Components/Logo/Logo.js'
-import ImageLink from './Components/ImageLink/ImageLink.js'
+import Navigation from './Components/Navigation/Navigation.js';
+import Logo from './Components/Logo/Logo.js';
+import ImageLink from './Components/ImageLink/ImageLink.js';
+import Rank from './Components/Rank/Rank.js';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecog from './Components/FaceRecog/FaceRecog.js';
 
+
+const app = new Clarifai.App({
+ apiKey: '8121cd4b17634bfa93295385c6ac9b49'
+});
+
+const particlesOptions = {
+	particles: {
+		number: {
+			value: 100,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+	}
+}
 class App extends Component {
+  constructor() {
+	  super();
+	  this.state = {
+		  input: '',
+		  imageUrl: ''
+	  }
+  }
+  
+  onInputChange = (event) => {
+	  this.setState({ input: event.target.value });
+  }
+  
+  onButtonSubmit = () => {
+	this.setState({imageUrl: this.state.input});
+	app.models.predict(
+	Clarifai.FACE_DETECT_MODEL,
+		this.state.input
+	)
+	.then(function(response) {
+		console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+		},
+	function(err) {}
+	);
+  }
+  
   render() {
     return (
       <div className="App">
+		<Particles className='particles' params={particlesOptions}/>
 		<Navigation />
 		<Logo />
-		<ImageLink />
-		{/*
-		
-		<FaceRecog />*/}
+		<Rank />
+		<ImageLink onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>	
+		<FaceRecog imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -6,7 +6,7 @@ import './Logo.css';
 const Logo = () => {
 	return(
 		<div className='center ma4 mt0'>
-			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 250, width: 250 }} >
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 200, width: 200 }} >
 				<div className="Tilt-inner pa3">	
 					<img style={{paddingTop: '5px'}} alt='chip' src={chip}/>
 				</div>
